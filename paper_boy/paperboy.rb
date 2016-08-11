4ac2ec0b07c026f172b32163520bac2318fab32a
Shane <shanebeehler@me.com>
2016-08-11 13:39:53 -0400

    Created Paper_boy class and give it instance variables.

---
+++ paper_boy/paperboy.rb
@@ -0,0 +1,11 @@
+class Paper_boy
+
+attr_reader :earnings
+
+  def initialize(name, experience, side, earnings)
+    @name = name
+    @experience = experience
+    @side = side
+    @earnings = earnings
+  end
+end
