b6611880e8469f9b05caa44e699fb9667d481c49
Shane <shanebeehler@me.com>
2016-08-11 16:39:18 -0400

    Created move method.

--- mars_rover/mars_rover.rb
+++ mars_rover/mars_rover.rb
@@ -1,10 +1,22 @@
 class Rover
 
-  attr_writer :read_instructions
-
   def initialize(x, y, dir)
     @x = x
     @y = y
     @dir = dir
   end
+
+  def read_instruction
+
+  end
+
+  def move
+    case dir
+      when "N" then @y + 1
+      when "S" then @y - 1
+      when "E" then @x + 1
+      when "W" then @x - 1
+    end
+  end
+  
 end
