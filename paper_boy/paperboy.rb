127041547645252ed583916096220492ae6f0614
Shane <shanebeehler@me.com>
2016-08-11 15:15:12 -0400

    Created deliver method.

--- paper_boy/paperboy.rb
+++ paper_boy/paperboy.rb
@@ -10,6 +10,25 @@ class Paper_boy
   end
 
   def quota
-    (@experience / 2) + 50
+    @quota = (@experience / 2) + 50
+  end
+
+  def deliver(start_address, end_address)
+
+    papes_delivered = end_address - start_address
+
+    start_pay = papes_delivered * 0.25
+
+    if papes_delivered < @quota
+      start_pay -= 2
+      return start_pay
+    elsif papes_delivered == quota
+      return start_pay
+    else
+      puts "over_pay"
+
+      over_pay = (papes_delivered - quota) * 0.25 + start_pay
+      return over_pay
+    end
   end
 end
